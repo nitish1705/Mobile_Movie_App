19c1ad287ac6c77d514b2fbc46f1af960bd66b45
Nitish M <mnitish1705@gmail.com>
2026-01-15 23:35:54 +0530

    Secong commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,13 +2,24 @@
 import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
-import { Image, ImageBackground, ScrollView, Text, useWindowDimensions, View } from "react-native";
+import { ActivityIndicator, FlatList, Image, ImageBackground, ScrollView, Text, useWindowDimensions, View } from "react-native";
 import { router, useRouter } from "expo-router";
+import useFetch from "@/services/useFetch";
+import { fetchMovies } from "@/services/api";
+import MovieCard from "@/components/MovieCard";
 
 export default function Index() {
   const { width, height } = useWindowDimensions();
   const router = useRouter();
   const marginLogoTop = height * 0.1;
+
+  const { 
+    data: movies, 
+    loading: moviesLoading, 
+    error : moviesError
+  } = useFetch(() => fetchMovies({
+    query: ''
+  }))
 
   return (
     <View className="flex-1 bg-primary">
@@ -21,13 +32,46 @@ export default function Index() {
         <Image source={icons.logo} resizeMode="contain"/>
       </View>
       <ScrollView className="flex-1">
-        <View className="flex-1 mt-5">
-          <SearchBar 
-            onPress={() => router.push("/search")}
-            placeholder = "Search for a Movie!"
+          {moviesLoading ? (
+            <ActivityIndicator 
+              size="large"
+              color = "#000ff"
+              className="mt-10 self-center"
+            />
+          ) : moviesError ? (
+            <Text>Error : {moviesError?.message}</Text>
+          ) : (
+            <View className="flex-1 mt-5">
+              <SearchBar 
+                onPress={() => router.push("/search")}
+                placeholder = "Search for a Movie!"
 
-          />
-        </View>
+              />
+              <>
+                <Text className="text-lg font-bold text-white mt-3 ml-3 mb-2">Latest Movies</Text>
+
+                <FlatList 
+                    data = {movies}
+                    renderItem = {({ item }) => (
+                        <MovieCard 
+                          {... item}
+                        />
+                        
+                    )}
+                    keyExtractor={(item) => item.id.toString()}
+                    numColumns={3}
+                    columnWrapperStyle={{
+                      justifyContent: 'flex-start',
+                      gap: 20,
+                      marginBottom: 10
+
+                    }}
+                    scrollEnabled={false}
+                />
+              </>
+            </View>
+          )
+        }
       </ScrollView>
     </View>
   );
---
+++ services/api.ts
@@ -0,0 +1,44 @@
+export const TMDB_CONFIG = {
+  BASE_URL: "https://api.themoviedb.org/3",
+  API_KEY: process.env.EXPO_PUBLIC_MOVIE_API_KEY,
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`,
+  }
+}
+
+export const fetchMovies = async ({ query } : {query: string}) => {
+    const endpoint = query
+    ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+    : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity_desc`;
+
+    const response = await fetch(endpoint, {
+        method : 'GET',
+        headers: TMDB_CONFIG.headers,
+    })
+
+    if(!response.ok){
+        // @ts-ignore
+        throw new Error('Failed to fetch Movies', response.statusText);
+        
+    }
+
+    const data = await response.json();
+
+    return data.results;
+}
+
+
+// const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     accept: 'application/json',
+//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0OTY2MDkxYmJlMGY4ZTBhYzhiZmUxNzU2MjdkNDk1ZSIsIm5iZiI6MTc2ODM2NzU0OC41NCwic3ViIjoiNjk2NzI1YmMzYWU4NWQ5NGE3ODY2MGViIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9._zdp1dVhgKP3fZhZ79z57fRYzFRlIK-Sc11hnNZqTFs'
+//   }
+// };
+
+// fetch(url, options)
+//   .then(res => res.json())
+//   .then(json => console.log(json))
+//   .catch(err => console.error(err));
